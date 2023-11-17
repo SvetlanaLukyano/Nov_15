86735508a38c3bcfd3cc126553003b10a7c9fe89
SvetlanaLukyano <gaechkadale@yandex.ru>
2023-11-17 19:39:06 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList
+        Scanner scanner = new Scanner(System.in);
+        //ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.println("PLease set K - the length of your array");
+        int k = scanner.nextInt();
+        System.out.println("Please enter your array elements");
+        int arr[] = new int[k];
+        boolean resume = false;
+
+        for (int i = 0; i < k; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < k; i++) {
+            if (arr[i] == arr[i + 1]) {
+                resume = true;
+                System.out.println(resume);
+            } else {
+                resume = false;
+                System.out.println(resume);
+            }
+        }
+        if (resume == false) System.out.println("No");
+        else {
+            System.out.println("Yes");
+        }
+        }
     }
-}
+
+
+
